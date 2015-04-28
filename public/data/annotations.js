e6756852aa2d543f31caa0992916c58c89e18766
Luc Pestille <Luc@project14.co.uk>
2015-04-28 16:28:55 +0100

    Headings.
    Major update to headings to remove duplicate styling and simplify
    classes. Annotations added for new heading dimensions. Added basic
    video embed.

--- public/data/annotations.js
+++ public/data/annotations.js
@@ -19,6 +19,51 @@ var comments = {
 		"el": "div[role=form-rules]",
 		"title" : "Forms",
 		"comment": "Old guidelines exist on the wiki; <a href='https://github.com/ThreeUK/digital-library.three.co.uk/wiki/Forms'>Forms</a>."
+	},
+	{
+		"el": "h1.u-text-size-000",
+		"title": "Double oversized heading",
+		"comment": "Font should render across the three breakpoints as 64px / 96px / 112px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 25px / 38px / 44px, across the three breakpoints." 
+	},
+	{
+		"el": "h1.u-text-size-0",
+		"title": "Oversized heading",
+		"comment": "Font should render across the three breakpoints as 44px / 64px / 72px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 17px / 25px / 28px, across the three breakpoints." 
+	},
+	{
+		"el": "h1",
+		"title": "h1",
+		"comment": "Font should render across the three breakpoints as 38px / 52px / 60px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 15px / 20px / 24px, across the three breakpoints." 
+	},
+	{
+		"el": "h2",
+		"title": "h2",
+		"comment": "Font should render across the three breakpoints as 30px / 42px/ 46px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 12px / 16px / 18px, across the three breakpoints." 
+	},
+	{
+		"el": "h3",
+		"title": "h3",
+		"comment": "Font should render across the three breakpoints as 26px / 34px / 36px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 10px / 13px / 14px, across the three breakpoints." 
+	},
+	{
+		"el": "h4",
+		"title": "h4",
+		"comment": "Font should render across the three breakpoints as 22px / 26px / 28px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 8px / 10px / 11px, across the three breakpoints." 
+	},
+	{
+		"el": "h5",
+		"title": "h5",
+		"comment": "Font should render across the three breakpoints as 18px / 20px / 22px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 7px / 8px / 8px, across the three breakpoints." 
+	},
+	{
+		"el": "h6",
+		"title": "h6",
+		"comment": "Font should render across the three breakpoints as 16px, with a line height at 115%. Bottom margin is set at 0.4em, which equates to 6px / 6px / 6px, across the three breakpoints." 
+	},
+	{
+		"el": ".u-text-meek",
+		"title": "Meek text",
+		"comment": "Headings are bold by default, but can have a lighter font style applied to included supplimental pieces of text - use sparingly!" 
 	}
 ]
 };
